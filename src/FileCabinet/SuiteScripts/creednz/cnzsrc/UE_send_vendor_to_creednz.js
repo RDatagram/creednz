857a32e07a7319bd9fe902dbee0a1febc9d901e7
RDatagram <zoran.r@rdata.rs>
2024-09-18 13:52:41 +0200

    Refactor and expand vendor DTO building process
    Added a new function, `buildAnalyzeVendorDtoFromRecord`, to centralize the construction of the vendorDTO object from a vendor record. Integrated this function into `UE_send_vendor_to_creednz.js` while cleaning up and consolidating redundant code. Enhanced `vendorDTO.json` to match the new schema specs for improved data integrity and extensibility.

--- src/FileCabinet/SuiteScripts/creednz/cnzsrc/UE_send_vendor_to_creednz.js
+++ src/FileCabinet/SuiteScripts/creednz/cnzsrc/UE_send_vendor_to_creednz.js
@@ -16,6 +16,7 @@ define(['N/runtime', 'N/log', 'N/record', 'N/error', 'N/render', 'N/file', 'N/ht
                 let currentRecId = context.newRecord.id;
                 log.debug("currentRecId", currentRecId);
                 log.debug("current record", currentRec);
+
                 let currentRecord = record.load({
                     type: record.Type.VENDOR,
                     id: currentRecId,
@@ -27,34 +28,35 @@ define(['N/runtime', 'N/log', 'N/record', 'N/error', 'N/render', 'N/file', 'N/ht
                  });*/
                 let vendorArray = [];
                 let vendorObj = {};
+                vendorObj = creednz_api_lib.buildAnalyzeVendorDtoFromRecord(currentRecord);
+
+                /*
                 let vendorName = currentRecord.getValue({
                     fieldId: "entityid"
                 });
                 if (vendorName) {
                     vendorObj.name = vendorName;
                 }
-                /* var vendorPrimaryContact = currentRecord.getValue({
-                    fieldId: "contact"
-                 });*/
-                let vendorCurrency = currentRecord.getValue({
-                    fieldId: "currency"
-                });
-                // vendorCurrency = JSON.stringify(vendorCurrency);
-                if (vendorCurrency) {
-                    vendorObj.currency = vendorCurrency;
+
+                let vendorRecId = JSON.stringify(currentRecId);
+                if (vendorRecId) {
+                    vendorObj.internalId = vendorRecId;
                 }
-                let vendorPhone = currentRecord.getValue({
-                    fieldId: "phone"
+
+                let vendorCreednzRegCode = currentRecord.getValue({
+                    fieldId: "custentity_registraion_code_creednz"
                 });
-                if (vendorPhone) {
-                    vendorObj.phone = vendorPhone;
+                if (vendorCreednzRegCode) {
+                    vendorObj.registrationCode = vendorCreednzRegCode;
                 }
+
                 let vendorSubsidiary = currentRecord.getValue({
                     fieldId: "subsidiary"
                 });
                 if (vendorSubsidiary) {
                     vendorObj.primarySubsidiary = vendorSubsidiary;
                 }
+
                 let vendorCreatedDate = currentRecord.getValue({
                     fieldId: "datecreated"
                 });
@@ -69,6 +71,7 @@ define(['N/runtime', 'N/log', 'N/record', 'N/error', 'N/render', 'N/file', 'N/ht
                 if (vendordateCreated1) {
                     vendorObj.dateCreated = vendordateCreated1;
                 }
+
                 //  var vendordateCreated1 = new Date(vendorCreatedDate);
                 let vendorLastModified = currentRecord.getValue({
                     fieldId: "lastmodifieddate"
@@ -84,12 +87,136 @@ define(['N/runtime', 'N/log', 'N/record', 'N/error', 'N/render', 'N/file', 'N/ht
                 if (lastModified2) {
                     vendorObj.lastModified = lastModified2;
                 }
+
                 let vendorEmail = currentRecord.getValue({
                     fieldId: "email"
                 });
                 if (vendorEmail) {
                     vendorObj.email = vendorEmail;
                 }
+
+                let vendorPhone = currentRecord.getValue({
+                    fieldId: "phone"
+                });
+                if (vendorPhone) {
+                    vendorObj.phone = vendorPhone;
+                }
+
+                let vendorCreednzPaymentMethod = currentRecord.getValue({
+                    fieldId: "custentity_vendor_payment_method_creeedn"
+                });
+                if (vendorCreednzPaymentMethod) {
+                    vendorObj.vendorPaymentMethod = vendorCreednzPaymentMethod;
+                }
+
+                let vendorCurrency = currentRecord.getValue({
+                    fieldId: "currency"
+                });
+                // vendorCurrency = JSON.stringify(vendorCurrency);
+                if (vendorCurrency) {
+                    vendorObj.currency = vendorCurrency;
+                }
+
+                let vendorCreednzBankAccName = currentRecord.getValue({
+                    fieldId: "custentity_bank_account_name_creednz"
+                });
+                if (vendorCreednzBankAccName) {
+                    vendorObj.bankAccountName = vendorCreednzBankAccName;
+                }
+
+                let vendorCreednzBankNumber = currentRecord.getValue({
+                    fieldId: "custentity_bank_number_creednz"
+                });
+                if (vendorCreednzBankNumber) {
+                    vendorObj.bankNumber = vendorCreednzBankNumber;
+                }
+
+                let vendorCreednzBranchName = currentRecord.getValue({
+                    fieldId: "custentity_branch_name_creednz"
+                });
+                if (vendorCreednzBranchName) {
+                    vendorObj.branchName = vendorCreednzBranchName;
+                }
+
+                let vendorCreednzBranchNumber = currentRecord.getValue({
+                    fieldId: "custentity_branch_number_creednz"
+                });
+                if (vendorCreednzBranchNumber) {
+                    vendorObj.branchNumber = vendorCreednzBranchNumber;
+                }
+
+                let vendorCreednzBankCode = currentRecord.getValue({
+                    fieldId: "custentity_bank_code_creednz"
+                });
+                if (vendorCreednzBankCode) {
+                    vendorObj.bankCode = vendorCreednzBankCode;
+                }
+
+                let vendorCreednzBankAccNumber = currentRecord.getValue({
+                    fieldId: "custentity_bank_account_number_creednz"
+                });
+                if (vendorCreednzBankAccNumber) {
+                    vendorObj.bankAccountNumber = vendorCreednzBankAccNumber;
+                }
+
+                let vendorCreednzBankAddress = currentRecord.getValue({
+                    fieldId: "custentity_bank_address_creednz"
+                });
+                if (vendorCreednzBankAddress) {
+                    vendorObj.bankAddress = vendorCreednzBankAddress;
+                }
+
+                let vendorCreednzBankDetailsUpdate = currentRecord.getValue({
+                    fieldId: "custentity_bank_details_update_creednz"
+                });
+                if (vendorCreednzBankDetailsUpdate) {
+                    vendorObj.bankDetailsUpdate = vendorCreednzBankDetailsUpdate;
+                }
+
+                let vendorCreednzEftBankUpdate = currentRecord.getValue({
+                    fieldId: "custentity_eft_bank_detailsupdate_creedn"
+                });
+                if (vendorCreednzEftBankUpdate) {
+                    vendorObj.eftBankDetailsUpdate = vendorCreednzEftBankUpdate;
+                }
+
+                let vendorCreednzEftBillPayment = currentRecord.getValue({
+                    fieldId: "custentity_eft_bill_payment_creednz"
+                });
+                if (vendorCreednzEftBillPayment) {
+                    vendorObj.eftBillPayment = vendorCreednzEftBillPayment;
+                }
+
+
+                let vendorCreednzIban = currentRecord.getValue({
+                    fieldId: "custentity_iban_creednz"
+                });
+                if (vendorCreednzIban) {
+                    vendorObj.iban = vendorCreednzIban;
+                }
+
+                let vendorCreednzSwift = currentRecord.getValue({
+                    fieldId: "custentity_swift_creednz"
+                });
+                if (vendorCreednzSwift) {
+                    vendorObj.swift = vendorCreednzSwift;
+                }
+
+                let vendorCreednzRoutingNumber = currentRecord.getValue({
+                    fieldId: "custentity_routing_number_creednz"
+                });
+                if (vendorCreednzRoutingNumber) {
+                    vendorObj.routingNumber = vendorCreednzRoutingNumber;
+                }
+
+                let vendorCreednzPaypalAcc = currentRecord.getValue({
+                    fieldId: "custentity_paypal_account_creednz"
+                });
+                if (vendorCreednzPaypalAcc) {
+                    vendorObj.paypalAccount = vendorCreednzPaypalAcc;
+                }
+
+
                 let vendorAddress = currentRecord.getValue({
                     fieldId: "billaddressee"
                 });
@@ -116,10 +243,11 @@ define(['N/runtime', 'N/log', 'N/record', 'N/error', 'N/render', 'N/file', 'N/ht
                     vendorObj.billingZip = vendorBillZip;
                 }
 
-                // TODO: why is empty? Always new externalID will be created?
-                /* var vendorExternalId = currentRec.getValue({
-                           fieldId: "custentity_vendor_external_id"
-                      });*/
+                /!* var vendorPrimaryContact = currentRecord.getValue({
+                    fieldId: "contact"
+                 });*!/
+
+
                 let vendorCreednzRisk = currentRecord.getValue({
                     fieldId: "custentity_creednz_risk_status"
                 });
@@ -127,102 +255,8 @@ define(['N/runtime', 'N/log', 'N/record', 'N/error', 'N/render', 'N/file', 'N/ht
                 let vendorCreednzLastUpdated = currentRecord.getValue({
                     fieldId: "custentity_creednz_updated_on"
                 });
-                let vendorRecId = JSON.stringify(currentRecId);
-                if (vendorRecId) {
-                    vendorObj.internalId = vendorRecId;
-                }
+*/
 
-
-                let vendorCreednzPaymentMethod = currentRecord.getValue({
-                    fieldId: "custentity_vendor_payment_method_creeedn"
-                });
-                if (vendorCreednzPaymentMethod) {
-                    vendorObj.vendorPaymentMethod = vendorCreednzPaymentMethod;
-                }
-                let vendorCreednzBankAccName = currentRecord.getValue({
-                    fieldId: "custentity_bank_account_name_creednz"
-                });
-                if (vendorCreednzBankAccName) {
-                    vendorObj.bankAccountName = vendorCreednzBankAccName;
-                }
-                let vendorCreednzBankAddress = currentRecord.getValue({
-                    fieldId: "custentity_bank_address_creednz"
-                });
-                if (vendorCreednzBankAddress) {
-                    vendorObj.bankAddress = vendorCreednzBankAddress;
-                }
-                let vendorCreednzBankCode = currentRecord.getValue({
-                    fieldId: "custentity_bank_code_creednz"
-                });
-                if (vendorCreednzBankCode) {
-                    vendorObj.bankCode = vendorCreednzBankCode;
-                }
-                let vendorCreednzBankNumber = currentRecord.getValue({
-                    fieldId: "custentity_bank_number_creednz"
-                });
-                if (vendorCreednzBankNumber) {
-                    vendorObj.bankAccountNumber = vendorCreednzBankNumber;
-                }
-                let vendorCreednzBankDetailsUpdate = currentRecord.getValue({
-                    fieldId: "custentity_bank_details_update_creednz"
-                });
-                if (vendorCreednzBankDetailsUpdate) {
-                    vendorObj.bankDetailsUpdate = vendorCreednzBankDetailsUpdate;
-                }
-                let vendorCreednzEftBankUpdate = currentRecord.getValue({
-                    fieldId: "custentity_eft_bank_detailsupdate_creedn"
-                });
-                if (vendorCreednzEftBankUpdate) {
-                    vendorObj.eftBankDetailsUpdate = vendorCreednzEftBankUpdate;
-                }
-                let vendorCreednzEftBillPayment = currentRecord.getValue({
-                    fieldId: "custentity_eft_bill_payment_creednz"
-                });
-                if (vendorCreednzEftBillPayment) {
-                    vendorObj.eftBillPayment = vendorCreednzEftBillPayment;
-                }
-                let vendorCreednzIban = currentRecord.getValue({
-                    fieldId: "custentity_iban_creednz"
-                });
-                if (vendorCreednzIban) {
-                    vendorObj.iban = vendorCreednzIban;
-                }
-                let vendorCreednzSwift = currentRecord.getValue({
-                    fieldId: "custentity_swift_creednz"
-                });
-                if (vendorCreednzSwift) {
-                    vendorObj.swift = vendorCreednzSwift;
-                }
-                let vendorCreednzRegCode = currentRecord.getValue({
-                    fieldId: "custentity_registraion_code_creednz"
-                });
-                if (vendorCreednzRegCode) {
-                    vendorObj.registrationCode = vendorCreednzRegCode;
-                }
-                let vendorCreednzBranchNumber = currentRecord.getValue({
-                    fieldId: "custentity_branch_number_creednz"
-                });
-                if (vendorCreednzBranchNumber) {
-                    vendorObj.branchNumber = vendorCreednzBranchNumber;
-                }
-                let vendorCreednzBranchName = currentRecord.getValue({
-                    fieldId: "custentity_branch_name_creednz"
-                });
-                if (vendorCreednzBranchName) {
-                    vendorObj.branchName = vendorCreednzBranchName;
-                }
-                let vendorCreednzRoutingNumber = currentRecord.getValue({
-                    fieldId: "custentity_routing_number_creednz"
-                });
-                if (vendorCreednzRoutingNumber) {
-                    vendorObj.routingNumber = vendorCreednzRoutingNumber;
-                }
-                let vendorCreednzPaypalAcc = currentRecord.getValue({
-                    fieldId: "custentity_paypal_account_creednz"
-                });
-                if (vendorCreednzPaypalAcc) {
-                    vendorObj.paypalAccount = vendorCreednzPaypalAcc;
-                }
                 //log.debug("vendorNameToSet",vendorNameToSet);
 
                 vendorArray.push(vendorObj);
--- src/FileCabinet/SuiteScripts/creednz/cnzsrc/lib/creednz_api_lib.js
+++ src/FileCabinet/SuiteScripts/creednz/cnzsrc/lib/creednz_api_lib.js
@@ -1,14 +1,14 @@
 /**
  * @NApiVersion 2.1
  */
-define(['N/https', 'N/record', 'N/search', './creednz_token_lib'],
+define(['N/https', 'N/record', 'N/search', './creednz_token_lib', 'N/format'],
     /**
      * @param{https} https
      * @param{record} record
      * @param{search} search
-     * @param{creednz_token_lib} creednz_token_lib
+     * @param{format} format
      */
-    (https, record, search, creednz_token_lib) => {
+    (https, record, search, creednz_token_lib,format) => {
 
         const baseCreednzPost = (endPoint,dataObj) => {
 
@@ -35,15 +35,246 @@ define(['N/https', 'N/record', 'N/search', './creednz_token_lib'],
                 details: JSON.stringify(creedNzResponse)
             })
             // check return code
-            if (creedNzResponse.code !== 200) {
+            if (creedNzResponse.code !== 201) {
                 log.error({
-                    title: 'Failed to post data to CreedNZ API',
+                    title: 'Response code from CreedNZ API',
                     details:JSON.stringify({endpoint:endPoint,responseCode:creedNzResponse.code})
                 });
             }
             return creedNzResponse;
         }
 
-        return {baseCreednzPost}
+        const buildAnalyzeVendorDtoFromRecord = (currentRecord) => {
+            let vendorObj = {
+                // code to build analyzeVendorDto from currentRecord
+            };
+            let vendorName = currentRecord.getValue({
+                fieldId: "entityid"
+            });
+            if (vendorName) {
+                vendorObj.name = vendorName;
+            }
+
+            let vendorRecId = JSON.stringify(currentRecord.id);
+            if (vendorRecId) {
+                vendorObj.internalId = vendorRecId;
+            }
+
+            let vendorCreednzRegCode = currentRecord.getValue({
+                fieldId: "custentity_registraion_code_creednz"
+            });
+            if (vendorCreednzRegCode) {
+                vendorObj.registrationCode = vendorCreednzRegCode;
+            }
+
+            let vendorSubsidiary = currentRecord.getValue({
+                fieldId: "subsidiary"
+            });
+            if (vendorSubsidiary) {
+                vendorObj.primarySubsidiary = vendorSubsidiary;
+            }
+
+            let vendorCreatedDate = currentRecord.getValue({
+                fieldId: "datecreated"
+            });
+            let vendordateCreatedFormat = format.format({
+                value: vendorCreatedDate,
+                type: format.Type.DATETIMETZ
+            });
+            let vendordateCreated1 = format.parse({
+                value: vendordateCreatedFormat,
+                type: format.Type.DATETIMETZ
+            });
+            if (vendordateCreated1) {
+                vendorObj.dateCreated = vendordateCreated1;
+            }
+
+            //  var vendordateCreated1 = new Date(vendorCreatedDate);
+            let vendorLastModified = currentRecord.getValue({
+                fieldId: "lastmodifieddate"
+            });
+            let lastModifiedFormat = format.format({
+                value: vendorLastModified,
+                type: format.Type.DATETIMETZ
+            });
+            let lastModified2 = format.parse({
+                value: lastModifiedFormat,
+                type: format.Type.DATETIMETZ
+            });
+            if (lastModified2) {
+                vendorObj.lastModified = lastModified2;
+            }
+
+            let vendorEmail = currentRecord.getValue({
+                fieldId: "email"
+            });
+            if (vendorEmail) {
+                vendorObj.email = vendorEmail;
+            }
+
+            let vendorPhone = currentRecord.getValue({
+                fieldId: "phone"
+            });
+            if (vendorPhone) {
+                vendorObj.phone = vendorPhone;
+            }
+
+            let vendorCreednzPaymentMethod = currentRecord.getValue({
+                fieldId: "custentity_vendor_payment_method_creeedn"
+            });
+            if (vendorCreednzPaymentMethod) {
+                vendorObj.vendorPaymentMethod = vendorCreednzPaymentMethod;
+            }
+
+            let vendorCurrency = currentRecord.getValue({
+                fieldId: "currency"
+            });
+            // vendorCurrency = JSON.stringify(vendorCurrency);
+            if (vendorCurrency) {
+                vendorObj.currency = vendorCurrency;
+            }
+
+            let vendorCreednzBankAccName = currentRecord.getValue({
+                fieldId: "custentity_bank_account_name_creednz"
+            });
+            if (vendorCreednzBankAccName) {
+                vendorObj.bankAccountName = vendorCreednzBankAccName;
+            }
+
+            let vendorCreednzBankNumber = currentRecord.getValue({
+                fieldId: "custentity_bank_number_creednz"
+            });
+            if (vendorCreednzBankNumber) {
+                vendorObj.bankNumber = vendorCreednzBankNumber;
+            }
+
+            let vendorCreednzBranchName = currentRecord.getValue({
+                fieldId: "custentity_branch_name_creednz"
+            });
+            if (vendorCreednzBranchName) {
+                vendorObj.branchName = vendorCreednzBranchName;
+            }
+
+            let vendorCreednzBranchNumber = currentRecord.getValue({
+                fieldId: "custentity_branch_number_creednz"
+            });
+            if (vendorCreednzBranchNumber) {
+                vendorObj.branchNumber = vendorCreednzBranchNumber;
+            }
+
+            let vendorCreednzBankCode = currentRecord.getValue({
+                fieldId: "custentity_bank_code_creednz"
+            });
+            if (vendorCreednzBankCode) {
+                vendorObj.bankCode = vendorCreednzBankCode;
+            }
+
+            let vendorCreednzBankAccNumber = currentRecord.getValue({
+                fieldId: "custentity_bank_account_number_creednz"
+            });
+            if (vendorCreednzBankAccNumber) {
+                vendorObj.bankAccountNumber = vendorCreednzBankAccNumber;
+            }
+
+            let vendorCreednzBankAddress = currentRecord.getValue({
+                fieldId: "custentity_bank_address_creednz"
+            });
+            if (vendorCreednzBankAddress) {
+                vendorObj.bankAddress = vendorCreednzBankAddress;
+            }
+
+            let vendorCreednzBankDetailsUpdate = currentRecord.getValue({
+                fieldId: "custentity_bank_details_update_creednz"
+            });
+            if (vendorCreednzBankDetailsUpdate) {
+                vendorObj.bankDetailsUpdate = vendorCreednzBankDetailsUpdate;
+            }
+
+            let vendorCreednzEftBankUpdate = currentRecord.getValue({
+                fieldId: "custentity_eft_bank_detailsupdate_creedn"
+            });
+            if (vendorCreednzEftBankUpdate) {
+                vendorObj.eftBankDetailsUpdate = vendorCreednzEftBankUpdate;
+            }
+
+            let vendorCreednzEftBillPayment = currentRecord.getValue({
+                fieldId: "custentity_eft_bill_payment_creednz"
+            });
+            if (vendorCreednzEftBillPayment) {
+                vendorObj.eftBillPayment = vendorCreednzEftBillPayment;
+            }
+
+
+            let vendorCreednzIban = currentRecord.getValue({
+                fieldId: "custentity_iban_creednz"
+            });
+            if (vendorCreednzIban) {
+                vendorObj.iban = vendorCreednzIban;
+            }
+
+            let vendorCreednzSwift = currentRecord.getValue({
+                fieldId: "custentity_swift_creednz"
+            });
+            if (vendorCreednzSwift) {
+                vendorObj.swift = vendorCreednzSwift;
+            }
+
+            let vendorCreednzRoutingNumber = currentRecord.getValue({
+                fieldId: "custentity_routing_number_creednz"
+            });
+            if (vendorCreednzRoutingNumber) {
+                vendorObj.routingNumber = vendorCreednzRoutingNumber;
+            }
+
+            let vendorCreednzPaypalAcc = currentRecord.getValue({
+                fieldId: "custentity_paypal_account_creednz"
+            });
+            if (vendorCreednzPaypalAcc) {
+                vendorObj.paypalAccount = vendorCreednzPaypalAcc;
+            }
+
+
+            let vendorAddress = currentRecord.getValue({
+                fieldId: "billaddressee"
+            });
+            if (vendorAddress) {
+                vendorObj.billingAddress = vendorAddress;
+            }
+
+            let vendorBillCountry = currentRecord.getValue({
+                fieldId: "billcountry"
+            });
+            if (vendorBillCountry) {
+                vendorObj.billingCountry = vendorBillCountry;
+            }
+            let vendorBillCity = currentRecord.getValue({
+                fieldId: "billcity"
+            });
+            if (vendorBillCity) {
+                vendorObj.billingCity = vendorBillCity;
+            }
+            let vendorBillZip = currentRecord.getValue({
+                fieldId: "billzip"
+            });
+            if (vendorBillZip) {
+                vendorObj.billingZip = vendorBillZip;
+            }
+
+            /* var vendorPrimaryContact = currentRecord.getValue({
+                fieldId: "contact"
+             });*/
+
+
+            let vendorCreednzRisk = currentRecord.getValue({
+                fieldId: "custentity_creednz_risk_status"
+            });
+
+            let vendorCreednzLastUpdated = currentRecord.getValue({
+                fieldId: "custentity_creednz_updated_on"
+            });
+
+            return vendorObj;
+        }
+        return {baseCreednzPost,buildAnalyzeVendorDtoFromRecord}
 
     });
